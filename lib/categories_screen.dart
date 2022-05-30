6f4118e2a10112633408a5ff0ad8934ceb13bbc1
Bandoss Ariss <arissthebandoss@gmail.com>
2022-05-30 20:06:43 +0100

    HomePage et page de catégories

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/category_item.dart';
+import './dummy_data.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: Text("DeliMeals")),
+        body: GridView(
+          padding: const EdgeInsets.all(20),
+          // ignore: prefer_const_constructors
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+            maxCrossAxisExtent: 300,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+          ),
+          // ignore: prefer_const_literals_to_create_immutables
+          children: DUMMY_CATEGORIES
+              .map((category) =>
+                  CategoryItem(category.id, category.title, category.color))
+              .toList(),
+        ));
   }
 }
